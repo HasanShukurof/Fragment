99d2e14a21753a6dd7587a2bc1177dc318d96f8d
Həsən Şükürov <Hesen2@Terminals.az>
2025-10-10 17:33:15 +0400

    fix

---
+++ app/src/main/java/com/example/learning/activity_and_fragment_learning/FirstActivity.kt
@@ -0,0 +1,22 @@
+package com.example.learning.activity_and_fragment_learning
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.learning.R
+import com.example.learning.databinding.ActivityFirstBinding
+import com.example.learning.databinding.ActivitySecondBinding
+
+class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
--- app/src/main/java/com/example/learning/activity_and_fragment_learning/SecondActivity.kt
+++ app/src/main/java/com/example/learning/activity_and_fragment_learning/SecondActivity.kt
@@ -1,19 +1,21 @@
-package com.example.learning
+package com.example.learning.activity_and_fragment_learning
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.learning.R
 import com.example.learning.databinding.ActivityMainBinding
+import com.example.learning.databinding.ActivitySecondBinding
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
